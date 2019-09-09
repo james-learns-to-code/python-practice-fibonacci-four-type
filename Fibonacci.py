7ef3f70b41b252501ce8978f15efa2026c1d33db
jameslee <jameslee@goodeffect.com>
2019-09-09 17:06:28 +0900

    Add new type 'tail call'

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,4 +1,4 @@
-# Mission : Make Fibonacci number using 3 type of approaching
+# Mission : Make Fibonacci number using 4 type of approaching
 
 # MARK: Implementation
 
@@ -6,6 +6,18 @@ def get_fib_num_by_recursive_at(number):
     if (number == 1 or number == 2):
         return 1
     return get_fib_num_by_recursive_at(number - 1) + get_fib_num_by_recursive_at(number - 2)
+
+def get_fib_num_by_recursive_tail_call_at(number):
+    if (number == 1 or number == 2):
+        return 1
+    return get_fib_num_by_recursive_tail_call(number, 0, 1)
+
+def get_fib_num_by_recursive_tail_call(number, first_number, sec_number):
+    if number == 0: 
+        return first_number 
+    if number == 1: 
+        return sec_number 
+    return get_fib_num_by_recursive_tail_call(number - 1, sec_number, first_number + sec_number)
 
 def get_fib_num_by_memoized_at(number):
     memo = {1: 1, 2: 1}
@@ -39,6 +51,12 @@ def test_get_fib_num_by_recursive_at(number, expect_number):
     print('fib_num is', fib_num)
     assert fib_num == expect_number, 'Error: Result is not expect'
 
+def test_get_fib_num_by_recursive_tail_call_at(number, expect_number):
+    print('test_get_fib_num_by_recursive_tail_call_at number: ', number)
+    fib_num = get_fib_num_by_recursive_tail_call_at(number)
+    print('fib_num is', fib_num)
+    assert fib_num == expect_number, 'Error: Result is not expect'
+
 def test_get_fib_num_by_memoized_at(number, expect_number):
     print('test_get_fib_num_by_memoized_at number: ', number)
     fib_num = get_fib_num_by_memoized_at(number)
@@ -53,7 +71,7 @@ def test_get_fib_num_by_bottom_up_at(number, expect_number):
 
 # Performance
 
-#@profile
+@profile
 def test_performance_get_fib_num_by_recursive():
     test_get_fib_num_by_recursive_at(2, 1)
     test_get_fib_num_by_recursive_at(5, 5)
@@ -61,7 +79,15 @@ def test_performance_get_fib_num_by_recursive():
     test_get_fib_num_by_recursive_at(20, 6765)
     test_get_fib_num_by_recursive_at(40, 102334155)
 
-#@profile
+@profile
+def test_performance_get_fib_num_by_recursive_tail_call():
+    test_get_fib_num_by_recursive_tail_call_at(2, 1)
+    test_get_fib_num_by_recursive_tail_call_at(5, 5)
+    test_get_fib_num_by_recursive_tail_call_at(10, 55)
+    test_get_fib_num_by_recursive_tail_call_at(20, 6765)
+    test_get_fib_num_by_recursive_tail_call_at(40, 102334155)
+
+@profile
 def test_performance_get_fib_num_by_memoized():
     test_get_fib_num_by_memoized_at(2, 1)
     test_get_fib_num_by_memoized_at(5, 5)
@@ -69,7 +95,7 @@ def test_performance_get_fib_num_by_memoized():
     test_get_fib_num_by_memoized_at(20, 6765)
     test_get_fib_num_by_memoized_at(40, 102334155)
 
-#@profile
+@profile
 def test_performance_get_fib_num_by_bottom_up():
     test_get_fib_num_by_bottom_up_at(2, 1)
     test_get_fib_num_by_bottom_up_at(5, 5)
@@ -80,5 +106,6 @@ def test_performance_get_fib_num_by_bottom_up():
 # MARK: Main
 
 test_performance_get_fib_num_by_recursive()
+test_performance_get_fib_num_by_recursive_tail_call()
 test_performance_get_fib_num_by_memoized()
 test_performance_get_fib_num_by_bottom_up()
